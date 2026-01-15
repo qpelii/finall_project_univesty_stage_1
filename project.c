d8afa64a9ca25c70066997746387e32c26b9fd7d
qpelii <qelichkhaniparsa@gmail.com>
2026-01-15 19:58:37 +0330

    set def print menu

--- project.c
+++ project.c
@@ -4,7 +4,43 @@
 #include<string.h>
 #define user_admin "admin"
 #define pass_admin "admin123456"
+
+void menu_login_print()
+{
+    char temp[25],line_char='|';
+    printf("Welcome to login page\n\n");
+    printf("---------------------------\n");
+    for(int i=0; i<4; i++)
+    {
+    printf("%c%-2d%c",line_char,i+1,line_char);
+    switch (i)
+    {
+    case 0:
+        strcpy(temp,"Admin Login page");
+        break;
+    case 1:
+        strcpy(temp,"Department Login page");
+        break;
+    case 2:
+        strcpy(temp,"Academic Login page");
+        break;
+    case 3:
+        strcpy(temp,"Exit program");
+        break;
+    default:
+        break;
+    }
+
+    printf("%-22s%c\n",temp,line_char);
+    if (i!=3)
+        printf("|--+----------------------|\n");
+
+
+    }
+    printf("---------------------------\n\n");
+    printf("select a option from menu: ");
+}
 void main()
 {
-
+    
 }
