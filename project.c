d0ffbfb5fe2b97d19dfd4518514a3af447f486b9
qpelii <qelichkhaniparsa@gmail.com>
2026-01-27 16:55:13 +0330

    add def seach student by location born

--- project.c
+++ project.c
@@ -4895,11 +4895,11 @@ void list_student_seach_major_print()
     } while (1);
     str_to_lower(major);
 
-    printf("Information about Student (Major: %s)\n________________________________________\n\n",temp_struct_student->major);
+    printf("Information about Students (Major: %s)\n________________________________________\n\n",temp_struct_student->major);
     int i=1;
     char temp[25];
     do{
-        if (strcmp(major,temp_struct_student->majorjor)==0 && strlen(major)==strlen(temp_struct_student->major))
+        if (strcmp(major,temp_struct_student->major)==0 && strlen(major)==strlen(temp_struct_student->major))
         {
             printf("Student's %d",i++);
             printf("\n-----------------------------------\n");
@@ -4936,6 +4936,65 @@ void list_student_seach_major_print()
     } while (temp[0]!=13);
     
 }
+}
+void list_student_seach_city_print()
+{
+    
+    char city[20];
+    printf("if you want cancel proses just press Enter\n");
+    printf("Enter Major of Students you want search: ");
+    do
+    {
+        gets(city);
+        if (strlen(city)==0)
+            return ;
+        else if (check_str_full_alpha_whit_space(city))
+            printf("Invalid from input!! try again: ");
+        else
+            break;
+    } while (1);
+    str_to_lower(city);
+
+    printf("Information about Students (Born location: %s)\n________________________________________\n\n",temp_struct_student->location_born);
+    int i=1;
+    char temp[25];
+    do{
+        if (strcmp(city,temp_struct_student->location_born)==0 && strlen(city)==strlen(temp_struct_student->location_born))
+        {
+            printf("Student's %d",i++);
+            printf("\n-----------------------------------\n");
+            strcpy(temp,"Name");
+            printf("%-20s%s\n",temp,temp_struct_student->name);
+            strcpy(temp,"Family name");
+            printf("%-20s%s\n",temp,temp_struct_student->family);
+            strcpy(temp,"ID University");
+            printf("%-20s%s\n",temp,temp_struct_student->ID_uni);
+            strcpy(temp,"Date Birthday");
+            printf("%-20s%s\n",temp,temp_struct_student->date_birthday);
+            strcpy(temp,"Major");
+            printf("%-20s%s\n",temp,temp_struct_student->major);
+            strcpy(temp,"ID code");// code meli
+            printf("%-20s%s\n",temp,temp_struct_student->ID_code);
+            strcpy(temp,"Phone number");
+            printf("%-20s%s\n",temp,temp_struct_student->phone_num);
+            strcpy(temp,"Email");
+            printf("%-20s%s\n",temp,temp_struct_student->email);
+            printf("--------------------------------------\n");
+        }
+        temp_struct_student=temp_struct_student->link;
+    }while(temp_struct_student!=NULL);
+    if (i==1)
+    {
+        printf("location born None of student is %s!\n",city);
+        printf("--------------------------------------\n");
+    }
+    printf("Press Enter to back list\n");
+    free_student();
+    do
+    {
+        temp[0]=getch();
+    } while (temp[0]!=13);
+    
 }
 
 void main()
